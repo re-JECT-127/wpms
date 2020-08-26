03bec7815ff1cade085ea9a3f589cefce3e40efa
reject <reject@pop-os.localdomain>
2020-08-26 14:43:18 +0300

    bababooie

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable max-len */
 
-export default function App() {
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+import List from './components/List';
+
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <List></List>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 37,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cats: {
+    padding: 10,
+    backgroundColor: 'grey',
+    flexDirection: 'row',
+  },
+  topic: {
+    fontSize: 30,
+  },
+  text: {
+    flex: 1,
+    padding: 20,
+    flexDirection: 'column',
+    alignSelf: 'center',
+  },
 });
+
+export default App;
